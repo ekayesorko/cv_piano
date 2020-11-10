20f71c8004a959318f0b65194455555dddf6cbd3
ekayesorko <ekayesorko@gmail.com>
2020-11-11 00:39:52 +0600

    added key class

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ while(True):
     ret, frame = cap.read()
     frame = frame[150:300, 120:390]
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.GaussianBlur(frame,(15,15),0)
+    frame = cv2.GaussianBlur(frame, (15, 15), 0)
     retval, frame = cv2.threshold(frame, 150, 255, cv2.THRESH_BINARY)
     cv2.imshow('blur', frame)
     if cv2.waitKey(1) & 0xFF == ord('a'):
